4c4144b26ec0b50d46be1c50e7210994080f0ba9
Nikitin Ilya <F1uctus@users.noreply.github.com>
2018-02-25 01:19:59 +0300

    Changed structure

---
+++ Axion/Tokens/Token.cs
@@ -0,0 +1,26 @@
+﻿namespace Axion.Tokens
+{
+    internal class Token
+    {
+        public readonly TokenType Type = TokenType.Unknown;
+        public readonly string Value;
+
+        internal Token() { }
+
+        internal Token(TokenType type)
+        {
+            Type = type;
+        }
+
+        internal Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value == null ? $"({Type})" : $"({Type:G}, '{Value}')";
+        }
+    }
+}
---
+++ Axion/Tokens/TokenType.cs
@@ -0,0 +1,32 @@
+﻿namespace Axion.Tokens
+{
+    internal enum TokenType
+    {
+        Unknown,
+
+        // Special operators:
+        OpenParenthese,
+        CloseParenthese,
+        Comma,
+        Semicolon,
+        Indent,
+        Outdent,
+        Newline,
+
+        // Types:
+        Number_Float,
+        Number_SFloat,
+        Number_LFloat,
+        Number_Int,
+        Number_SInt,
+        Number_LInt,
+        Number_Byte,
+        String,
+        Identifier,
+
+        // Other parts:
+        Operator,
+        Reference,
+        Keyword
+    }
+}
